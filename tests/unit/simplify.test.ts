4a603f4bbc06bcf963422a61b5f4a33112f26d36
florian23marc <florian.marc.pro@gmail.com>
2026-05-09 21:41:20 +0200

    RED: add 3-person and 4-person simplifyDebts tests

--- tests/unit/simplify.test.ts
+++ tests/unit/simplify.test.ts
@@ -6,4 +6,28 @@ describe('simplifyDebts', () => {
     const settlements = simplifyDebts({ a: 10, b: -10 });
     expect(settlements).toEqual([{ from: 'b', to: 'a', amount: 10 }]);
   });
+
+  it('should settle a 3-person triangle with one settlement', () => {
+    const settlements = simplifyDebts({ a: 10, b: 0, c: -10 });
+    expect(settlements).toEqual([{ from: 'c', to: 'a', amount: 10 }]);
+  });
+
+  it('should minimize the number of settlements for a 4-person circular debt', () => {
+    const settlements = simplifyDebts({ a: 30, b: -20, c: -10, d: 0 });
+    expect(settlements).toEqual([
+      { from: 'b', to: 'a', amount: 20 },
+      { from: 'c', to: 'a', amount: 10 },
+    ]);
+  });
+
+  it('should ignore zero balances and still compute correct settlements', () => {
+    const settlements = simplifyDebts({ a: 15, b: -5, c: -10, d: 0 });
+    expect(settlements).toHaveLength(2);
+    expect(settlements).toEqual(
+      expect.arrayContaining([
+        { from: 'b', to: 'a', amount: 5 },
+        { from: 'c', to: 'a', amount: 10 },
+      ]),
+    );
+  });
 });
